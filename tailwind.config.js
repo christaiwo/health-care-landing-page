8eeb063888da75bc0f7e8d2fd52a72334b021dbf
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-04-09 12:18:02 +0100

    Added reviews  section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
     extend: {
       height: {
         '354': '354px',
+        '300': '300px',
       },
       width: {
         '350': '350px',
         '300': '300px',
         '250': '250px',
+        '1000' : '1000px'
       },
       fontSize: {
         '36': '36px',
