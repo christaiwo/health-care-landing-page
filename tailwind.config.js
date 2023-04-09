46b62b997836669ef8e747d3294e53df5c015849
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-04-09 09:23:17 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      height: {
+        '354': '354px',
+      },
+      width: {
+        '350': '350px',
+      }
+    },
+  },
+  plugins: [],
+}
+
